22a16e21fb2d518042adc6196770c5e32353fefc
kurianzz <32818075+kurianzz@users.noreply.github.com>
2023-07-21 07:46:54 +0000

    copilot commit 3

---
+++ comments.js
@@ -0,0 +1,26 @@
+// create a web server
+// 1. create a web server
+// 2. read the file that contains the comments
+// 3. create a route to handle comments
+
+// 1. create a web server
+const express = require('express')
+const app = express()
+const port = 3000
+
+// 2. read the file that contains the comments
+const fs = require('fs')
+const comments = JSON.parse(fs.readFileSync('./comments.json', 'utf8'))
+
+// 3. create a route to handle comments
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/comments', (req, res) => {
+  res.send(comments)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
